576b0bcd75bf06097051c429d172436fc8d07b07
gnanadeepika09 <chaparalagnanadeepika@gmail.com>
2024-08-07 10:28:08 +0530

    Add files via upload

---
+++ EvenandOdd.java
@@ -0,0 +1,18 @@
+class EvenandOdd{
+ public static void main(String args[])
+ {
+  int a[]={3,8,4,1,9,56};
+  int i;
+  for(i=0;i<a.length;i++)
+  {
+   if(a[i]%2==0)
+    System.out.println(a[i]+"");
+  }
+  for(i=0;i<a.length;i++)
+  {
+   if(a[i]%2!=0)
+    System.out.println(a[i]+"");
+  }
+}
+}
+  
